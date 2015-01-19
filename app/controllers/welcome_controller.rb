5b0033d02274c10b1ccbbf94e8b4b02ea2740bf5
DoruMunteanu <dorumunteanu17@gmail.com>
2015-01-19 19:42:11 +0200

    send mail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,17 @@ class WelcomeController < ApplicationController
   end
 
   def send_mail
-    ContactMailer.sendmail()
+    mail = Mail.new do
+      from    'dorumunteanu17@gmail.com'
+      to      'wbenjamin1028@gmail.com'
+      subject 'This is a test email'
+      body    'abcdefghijmd'
+    end
+
+    mail.delivery_method :sendmail
+
+    mail.deliver
+
     render text: "abcd"
   end
 end
