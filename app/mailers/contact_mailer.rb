75a36f30354337fdb507c46fbee18e2c2ad1a749
DoruMunteanu <dorumunteanu17@gmail.com>
2015-01-20 18:02:15 +0200

    send mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ApplicationMailer
     @company = userinfo[:company]
     @email = userinfo[:mail]
     @package = userinfo[:package]
-    mail(to: "wbenjamin1028@gmail.com", from:@email, subject: 'Welcome to My Awesome Site')
+    mail(to: "info@lobolab.biz", from:@email, subject: 'Welcome to My Awesome Site')
   end
 end
